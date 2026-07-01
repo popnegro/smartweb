b822d40e9e5049971ae80f6875b440c1b586078b
popnegro <grasso.luis@gmail.com>
2026-07-01 01:29:16 -0300

    changePath

--- hub/next.config.js
+++ hub/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   // Para desplegar el sitio en una subcarpeta, cambia este valor.
   // Por ejemplo, si usas '/hub', tu sitio será accesible en `dominio.com/hub`.
   // Déjalo como un string vacío ('') si el sitio va en la raíz del dominio.
-  basePath: '/hub',
+  basePath: '',
 };
 
 export default nextConfig;
