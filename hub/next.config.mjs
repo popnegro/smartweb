01d05d900f325473b043dd72f3bc32050aa4873f
popnegro <grasso.luis@gmail.com>
2026-06-30 23:45:59 -0300

    /hub

---
+++ hub/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Para desplegar el sitio en una subcarpeta, cambia este valor.
+  // Por ejemplo, si usas '/hub', tu sitio será accesible en `dominio.com/hub`.
+  // Déjalo como un string vacío ('') si el sitio va en la raíz del dominio.
+  basePath: '/hub',
+};
+
+export default nextConfig;
